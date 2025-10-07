43c9cabe2f13df3e93e2a1903e9d7925843bfe04
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-10-07 12:42:35 +0200

    feat: P4ADEV-3963 adding superset (#1)

---
+++ .github/terraform/99_locals.tf
@@ -0,0 +1,35 @@
+locals {
+  # Repo
+  github = {
+    org        = "pagopa"
+    repository = "p4pa-analytics-deploy-aks"
+  }
+  domain = "payhub"
+
+  repo_secrets = var.env_short == "p" ? {
+  } : {}
+
+  map_repo = {
+    "dev" : "*",
+    "uat" : "uat"
+    "prod" : "main"
+  }
+
+  bypass_branch_rules_teams = ["p4pa-admins", "payments-cloud-admin"]
+
+  # this is use to lookup the id for each team
+  team_name_to_id = {
+    for team in data.github_organization_teams.all.teams :
+    team.name => team.id
+  }
+
+  branch_rulesets = {
+    main = {
+      ref_name                        = "refs/heads/main"
+      bypass_actors                   = true
+      required_linear_history         = true
+      require_code_owner_review       = true
+      required_approving_review_count = 1
+    },
+  }
+}
---
+++ .github/terraform/env/uat/terraform.tfvars
@@ -0,0 +1,12 @@
+prefix         = "p4pa"
+env            = "uat"
+env_short      = "u"
+location_short = "itn"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "UAT"
+  Owner       = "P4PA"
+  Source      = "https://github.com/pagopa/p4pa-analytics-deploy-aks"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
